95e3ebede405e36d51be6e5be53e8b2d86b69ecd
arjybltrn <arjaybeltran16@gmail.com>
2024-01-07 15:42:07 -0500

    adds styling to main and side-bar

--- src/App.js
+++ src/App.js
@@ -3,13 +3,33 @@ const App = () => {
     <div className="App">
       <section className='side-bar'> 
         <button> + New Chat </button>
-        <ul className='history'></ul>
+        <ul className='history'>
+            <li>asdasd</li>
+        </ul>
         <nav>
           <p>Made by Arjay</p>
         </nav>
       </section>
 
-      <section className='main'> </section>
+      <section className='main'> 
+        <h1>ArjayGPT</h1>
+        <ul className="feed">
+
+        </ul>
+
+        <div className="bottom-section">
+          <div className="input-container">
+            <input/>
+            <div id="submit"> ➤ </div>
+          </div>
+
+          <p className="info">
+            Chat GPT Mar 14 Version. Free Research Preview.
+            Our goal is to make AI systems more natural and safe to interact with.
+            Your feedback will help us improve.
+          </p>
+        </div>
+      </section>
     </div>
   )
 }
